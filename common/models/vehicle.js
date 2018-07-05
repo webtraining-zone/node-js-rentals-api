af2469e287cecd58c62f6723b1508c109ebbe391
Alex Arriaga <alexzander.arriaga@base22.com>
2018-07-05 16:20:11 -0500

    Agregar ejemplo de validador asíncrono custom

--- common/models/vehicle.js
+++ common/models/vehicle.js
@@ -9,4 +9,22 @@ module.exports = function(Vehicle) {
       min: `Name of vehicle should be at leats ${MIN_LENGHT_NAME} long.`,
     },
   });
+
+  Vehicle.validateAsync('manufacturerId', validateAsyncForManufacturerId, {});
+
+  // This function verifies the manufacturerId passed in the request exists
+  function validateAsyncForManufacturerId(error, next) {
+    // Get the model
+    // https://loopback.io/doc/en/lb3/Working-with-LoopBack-objects.html
+    const Manufacturer = Vehicle.app.models.Manufacturer;
+    Manufacturer.exists(this.manufacturerId,
+      function(errorExists, instanceExists) {
+        if (errorExists || !instanceExists) {
+          error();
+        }
+
+        // Everything is fine, proceed
+        next();
+      });
+  }
 };
