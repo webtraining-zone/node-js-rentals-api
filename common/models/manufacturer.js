f8fc3aee4e37aa0a232f531ec8e1805602e0693d
Alex Arriaga <alexzander.arriaga@base22.com>
2018-07-05 15:51:02 -0500

    Agregar ejemplos de validación out-of-the-box

--- common/models/manufacturer.js
+++ common/models/manufacturer.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = function(Manufacturer) {
+  // Validate uniqueness. Ensure the value of the property is unique for the model.
+  // Not available for all connectors. Currently supported with these connectors:
+  //
+  // - In Memory
+  // - Oracle
+  // - MongoDB
 
+  Manufacturer.validatesUniquenessOf('name',
+    {message: 'Manufacturer\'s name is not unique'});
 };
--- common/models/vehicle.js
+++ common/models/vehicle.js
@@ -2,4 +2,11 @@
 
 module.exports = function(Vehicle) {
   // Customize model behaviour here!
+  const MIN_LENGHT_NAME = 2;
+  Vehicle.validatesLengthOf('model', {
+    min: MIN_LENGHT_NAME,
+    message: {
+      min: `Name of vehicle should be at leats ${MIN_LENGHT_NAME} long.`,
+    },
+  });
 };
