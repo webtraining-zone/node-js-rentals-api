a96d58c053c4fd5125725c2a3daf0230f7a307ba
Alex Arriaga <alexzander.arriaga@base22.com>
2018-07-16 15:32:50 -0500

    Agregar ejemplo de migración para tabla de rentals

--- server/migration-scripts/create-rentals-tables.js
+++ server/migration-scripts/create-rentals-tables.js
@@ -4,12 +4,13 @@ const server = require('../server');
 
 // Be sure you use the DATASOURCE for authentication
 const dataSource = server.dataSources.mysql_db_auth;
-const lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role'];
+const lbTables = ['Rental'];
 
 // Taken from: https://loopback.io/doc/en/lb3/Creating-a-database-schema-from-models.html
 dataSource.automigrate(lbTables, function(error) {
   if (error) throw error;
-  console.log('Loopback tables [' + lbTables + '] created in ',
+
+  console.log('Rental table [' + lbTables + '] created in ',
     dataSource.adapter.name);
   dataSource.disconnect();
 });
